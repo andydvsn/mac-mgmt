bc8db83facf90e4995d46a00fe74e74a1c71703d
Andy Davison <andy@birdslikewires.co.uk>
2014-05-16 11:07:52 +0100

    Added watchdog section.
    Now you can run it while the system applies a point update.

--- bootefi/installbootefi.sh
+++ bootefi/installbootefi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# installbootefi.sh v1.01 (16th May 2014) by Andy Davison
+# installbootefi.sh v1.03 (16th May 2014) by Andy Davison
 #  Installs whatever boot.efi file you point it to into the right places.
 
 if [ "$USER" != "root" ]; then
@@ -8,13 +8,37 @@ if [ "$USER" != "root" ]; then
 	exit 0
 fi
 
-if [[ $# -ne 2 ]]; then
-	echo "Usage: $0 <target root> <boot.efi>"
+if [[ $# -lt 2 ]]; then
+	echo "Usage: $0 <target root> <boot.efi> [watchdog]"
 	exit 0
 fi
 
 TARGET="$1"
 BOOTEFI="$2"
+
+replace_bootefi() {
+
+	echo "Removing previous boot.efi files..."
+	rm -vf $TARGET/System/Library/CoreServices/boot.efi
+	rm -vf $TARGET/usr/standalone/i386/boot.efi
+	echo
+
+	echo "Copying boot.efi..."
+	cp -v "$BOOTEFI" $TARGET/System/Library/CoreServices/boot.efi
+	cp -v "$BOOTEFI" $TARGET/usr/standalone/i386/boot.efi
+	echo
+
+	echo "Setting permissions..."
+	chmod 644 $TARGET/System/Library/CoreServices/boot.efi
+	chmod 644 $TARGET/usr/standalone/i386/boot.efi
+	chown root:wheel $TARGET/System/Library/CoreServices/boot.efi
+	chown root:wheel $TARGET/usr/standalone/i386/boot.efi
+	xattr -c $TARGET/System/Library/CoreServices/boot.efi
+	xattr -c $TARGET/usr/standalone/i386/boot.efi
+
+	echo "Complete."
+
+} 
 
 CHECKEFI=0
 [ -f $TARGET/System/Library/CoreServices/boot.efi ] && CHECKEFI=$(($CHECKEFI+1))
@@ -26,37 +50,49 @@ if [[ $CHECKEFI -ne 2 ]]; then
 	exit 0
 fi
 
-echo "This will replace your boot.efi files with the one you have specified."
-echo "There is no undo. There is only do."
-echo
-echo -n "Are you sure? (y/n) "
-read SURE
-echo
+if [[ "$3" == "watchdog" ]]; then
 
-if [ "$SURE" == "y" ] || [ "$SURE" == "Y" ]; then
-	
-	echo "Removing previous boot.efi files..."
-	rm -f $TARGET/System/Library/CoreServices/boot.efi
-	rm -f $TARGET/usr/standalone/i386/boot.efi
+	echo "Exercising the watchdog..."
 
-	echo "Copying boot.efi..."
-	cp "$BOOTEFI" $TARGET/System/Library/CoreServices/boot.efi
-	cp "$BOOTEFI" $TARGET/usr/standalone/i386/boot.efi
+	EFIONETHEN=`stat -f %m $TARGET/System/Library/CoreServices/boot.efi`
+	EFITWOTHEN=`stat -f %m $TARGET/usr/standalone/i386/boot.efi`
 
-	echo "Setting permissions..."
-	chmod 644 $TARGET/System/Library/CoreServices/boot.efi
-	chmod 644 $TARGET/usr/standalone/i386/boot.efi
-	chown root:wheel $TARGET/System/Library/CoreServices/boot.efi
-	chown root:wheel $TARGET/usr/standalone/i386/boot.efi
-	xattr -c $TARGET/System/Library/CoreServices/boot.efi
-	xattr -c $TARGET/usr/standalone/i386/boot.efi
+	while true ; do
 
-	echo "Complete."
-	exit 0
+		EFIONENOW=`stat -f %m $TARGET/System/Library/CoreServices/boot.efi`
+		EFITWONOW=`stat -f %m $TARGET/usr/standalone/i386/boot.efi`
+
+		if [[ "$EFIONENOW" != "$EFIONETHEN" ]] || [[ "$EFITWONOW" != "$EFITWOTHEN" ]]; then
+
+			replace_bootefi
+			EFIONETHEN=`stat -f %m $TARGET/System/Library/CoreServices/boot.efi`
+			EFITWOTHEN=`stat -f %m $TARGET/usr/standalone/i386/boot.efi`
+
+		fi
+
+		sleep 1
+
+	done
 
 else
 
-	echo "Exited."
-	exit 0
-	
+	echo "This will replace your boot.efi files with the one you have specified."
+	echo "There is no undo. There is only do."
+	echo
+	echo -n "Are you sure? (y/n) "
+	read SURE
+	echo
+
+	if [ "$SURE" == "y" ] || [ "$SURE" == "Y" ]; then
+		
+		replace_bootefi
+		exit 0
+
+	else
+
+		echo "Exited."
+		exit 0
+		
+	fi
+
 fi
