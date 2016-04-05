de465e1df1db8cdb85272b1983b6b67c4b6ef80a
Andy Davison <admin@Titchy.fritz.box>
2016-04-05 22:39:17 +0100

    Sanity check, logging tweaks, safety switch.
    Been using the sanity check for a while, but realised I’d never
    actually synced it here. Oops!

--- eraseoldusers/eraseoldusers.pl
+++ eraseoldusers/eraseoldusers.pl
@@ -1,13 +1,18 @@
 #!/usr/bin/perl -w
 
-## eraseoldusers.pl v1.04 (28th August 2015) by Andy Davison.
+## eraseoldusers.pl v1.05 (5th April 2016) by Andy Davison.
 ##  Erases user directories that exceed the inactive timeout.
 
 use strict;
 use POSIX qw(strftime);
 
+## Is this script armed and dangerous?
+my $armed = 0;
 ## Number of days before we can consider something stale.
 my $killafter = 353;
+## If the directories are older than this, leave them. Check that the clock is correct.
+my $sanitycheck = 1000;
+
 my $verbose = 1;
 
 my $date = strftime "%a %b %e %H:%M:%S %Y", localtime;
@@ -22,17 +27,23 @@ if ($logsize > 102400) {
 }
 
 print "\nStarted: $date\n";
-print "\nBeginning removal of user accounts that have been inactive for $killafter days or more.\n\n";
+print "\nRemoving user accounts that have been inactive for $killafter days or more.\n\n";
 
 my $dfpre = qx{df -g /Users/};
 my @preaudiodrive = (split ' ', $dfpre);
 
 my @userlist = qx{ls -1 /Users/};
 
+# Flush these entries from the array.
 @userlist = grep(!/.localized/, @userlist);
 @userlist = grep(!/.DS_Store/, @userlist);
 @userlist = grep(!/admin/, @userlist);
 @userlist = grep(!/Shared/, @userlist);
+
+if (!@userlist) {
+	print "The user list is empty.\n\n";
+	exit 0
+}
 
 foreach my $oneuser (@userlist) {
 	
@@ -40,7 +51,7 @@ foreach my $oneuser (@userlist) {
 		
 	chomp($oneuser);
 	
-	print "Account $oneuser: " if ($verbose > 0);
+	print "$oneuser: " if ($verbose > 0);
 	
 	# When was the home directory itself last modified?
 	$homeage = -M "/Users/$oneuser";
@@ -65,10 +76,24 @@ foreach my $oneuser (@userlist) {
 	
 	if ($homeage > $killafter && $desktopage > $killafter && $docsage > $killafter) {
 		
-		print "I consider the account $oneuser to be inactive. It is being erased...";
-		qx{rm -rf /Users/$oneuser 2>/dev/null};
-		print " done.\n\n";
-		
+		print "$oneuser: Account inactive";
+
+		if ($homeage < $sanitycheck) {
+
+			if ($armed) {
+				print ". It is being erased...";
+				qx{rm -rf /Users/$oneuser 2>/dev/null};
+				print " done.\n\n";
+			} else {
+				print ". It would be gone now, if the safety had been off.\n\n"
+			}
+
+		} else {
+
+			print ", but it's age exceeded the sanity check. Is the system time correct?\n\n"
+
+		}
+
 	}
 		
 }
@@ -78,12 +103,13 @@ my @postaudiodrive = (split ' ', $dfpost);
 
 my $freed = $preaudiodrive[12] - $postaudiodrive[12];
 
-print "\n";
-print "  Before:  $preaudiodrive[13]GB available ($preaudiodrive[12]GB used).\n";
-print "   After:  $postaudiodrive[13]GB available ($postaudiodrive[12]GB used).\n\n";
+print "  Before:  $preaudiodrive[13]GB available ($preaudiodrive[12]GB used)\n";
+print "   After:  $postaudiodrive[13]GB available ($postaudiodrive[12]GB used)\n\n";
 
 if ($freed > 0) {
 	print "We've freed up ".$freed."GB!\n\n";
 } else {
 	print "Not managed to reclaim any space this time.\n\n"
 }
+
+exit 0
