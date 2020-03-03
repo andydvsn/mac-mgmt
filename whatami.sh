2186573622c8198dbf1f57af0656ba2fdae87550
Andrew Davison <andydvsn@users.noreply.github.com>
2020-03-03 22:25:09 +0000

    Update whatami

--- whatami.sh
+++ whatami.sh
@@ -1,9 +1,74 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ## whatami.sh v1.00 (9th October 2014)
-##  Displays the useful information about what a Mac actually is.
+##  Displays useful information about what a Mac actually is.
 
-IOREG=`ioreg -p IODeviceTree -r -n / -d 1`
-echo "$IOREG" | grep model | awk -F\"  {'print $4'}
-echo "$IOREG" | grep board-id | awk -F\"  {'print $4'}
-sysctl -n machdep.cpu.brand_string
+# Data Grabbers
+ioreg=$(ioreg -p IODeviceTree -r -n / -d 1)
+nvram=$(nvram -p)
+
+# Bootloader Identifier
+bootloader=$(nvram 4D1FDA02-38C7-4A6A-9CC6-4BCCA8B30102:opencore-version 2>&1)
+if [[ "$bootloader" =~ "not found" ]]; then
+	bootloader="Apple"
+else
+	bootloader="OpenCore $(echo "$bootloader" | awk -F\  '{print $2}')"
+fi
+
+# Hardware
+
+gpudata=$(system_profiler SPDisplaysDataType 2>/dev/null)
+
+if [[ "$(echo "$ioreg" | grep model | awk -F\"  {'print $4'})"" =~ "MacPro"" ]]; then
+	bay1="Empty"
+	bay2="Empty"
+	bay3="Empty"
+	bay4="Empty"
+	for d in "$(diskutil list | grep "/dev" | awk -F\  '{print $1}')"; do
+		#bay=$(diskutil info "$d" | grep Bay | awk -F\"Bay\  {'print $2'} | cut -b1)
+		#if [[ "$bay" != "" ]]; then
+		#	declare bay$bay="$d"
+		#fi
+		$(diskutil info "$d")
+		#echo "$d"
+	done
+fi
+
+
+
+# Output
+
+echo
+echo "                                                                  whatami v1.00"
+echo "==============================================================================="
+echo
+echo "Bootloader:   "$bootloader""
+echo
+echo "--- Hardware ------------------------------------------------------------------"
+echo
+echo "Model:        $(echo "$ioreg" | grep model | awk -F\"  {'print $4'})"
+echo "Board ID:     $(echo "$ioreg" | grep board-id | awk -F\"  {'print $4'})"
+echo
+echo "CPU:          $(sysctl -n machdep.cpu.brand_string)"
+echo "Virtualised:  $([[ $(sysctl -n machdep.cpu.features) =~ "VMM" ]] && echo "Virtual (VMM Present)" || echo "Physical (VMM Absent)")"
+echo
+echo "GPU:          $(echo "$gpudata" | grep "Chipset Model" | awk -F\:\  {'print $2'})"
+echo "VRAM:         $(echo "$gpudata" | grep "VRAM" | awk -F\:\  {'print $2'})"
+echo "Metal:        $(echo "$gpudata" | grep "Metal" | awk -F\:\  {'print $2'})"
+echo
+if [[ "$bay1" != "" ]]; then
+	echo "Drive Bay 1:  $bay1"
+	echo "Drive Bay 2:  $bay2"
+	echo "Drive Bay 3:  $bay3"
+	echo "Drive Bay 4:  $bay4"
+fi
+
+echo
+
+
+
+
+
+
+
+
